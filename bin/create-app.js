0161deabd43503db8133b70eff41a560a4af9cba
reeli <reeli.null@gmail.com>
2018-12-17 18:58:10 +0800

    support customize project name and update package.json

--- bin/create-app.js
+++ bin/create-app.js
@@ -4,6 +4,8 @@ const program = require("commander");
 const inquirer = require("inquirer");
 const download = require("download-git-repo");
 const ora = require('ora');
+const fs = require("fs");
+const path = require("path");
 
 program
   .version("0.0.0")
@@ -16,13 +18,29 @@ program
       name: "repo",
       message: "which repo do you want to install?",
       choices
+    }, {
+      type: "input",
+      name: "name",
+      message: "Please type the project name",
+      validate: function (input) {
+        const done = this.async();
+        if (input.length === 0) {
+          done("Must type the project name.");
+          return;
+        }
+        if (fs.existsSync(input)) {
+          done("The project name already exists, please type another one.");
+          return;
+        }
+        done(null, true);
+      }
     }];
 
-    const answers = await inquirer.prompt(questions);
-    const repo = answers.repo;
+    const {repo, name} = await inquirer.prompt(questions);
     const spinner = ora(`Downloading ${repo}...`);
+
     spinner.start();
-    download(`reeli/${repo}`, "./test", {clone: true}, (error) => {
+    download(`reeli/${repo}`, path.resolve(process.cwd(), name), {clone: true}, (error) => {
       error
         ? spinner.fail(`Downloaded ${repo} failed`)
         : spinner.succeed(`Downloaded ${repo} success`);
