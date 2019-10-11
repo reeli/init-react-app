dc88d9c1b49e226c0f29e10cd698b45f3d0b0972
reeli <reeli.null@gmail.com>
2019-10-11 21:40:50 +0800

    fix

--- bin/cli.js
+++ bin/cli.js
@@ -3,4 +3,53 @@
 const program = require("commander");
 program.parse(process.argv);
 
-require("../commands/" + program.args + ".js");
+const inquirer = require("inquirer");
+const download = require("download-git-repo");
+const ora = require("ora");
+const fs = require("fs");
+const path = require("path");
+
+program
+  .version("0.0.0")
+  .command("init")
+  .description("init project by github template repo")
+  .action(async () => {
+    const choices = ["simple", "complete"];
+    const questions = [
+      {
+        type: "list",
+        name: "repo",
+        message: "which repo do you want to install?",
+        choices,
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "Please type the project name",
+        validate: function(input) {
+          const done = this.async();
+          if (input.length === 0) {
+            done("Must type the project name.");
+            return;
+          }
+          if (fs.existsSync(input)) {
+            done("The project name already exists, please type another one.");
+            return;
+          }
+          done(null, true);
+        },
+      },
+    ];
+
+    const { repo, name } = await inquirer.prompt(questions);
+    const spinner = ora(`Downloading ${repo}...`);
+
+    spinner.start();
+    download(`reeli/react-sample-repo#${repo}`, path.resolve(process.cwd(), name), { clone: true }, (error) => {
+      error ? spinner.fail(`Downloaded ${repo} failed`) : spinner.succeed(`Downloaded ${repo} success`);
+      spinner.stop();
+    });
+  });
+
+// use parse method to invoking commands
+program.parse(process.argv);
